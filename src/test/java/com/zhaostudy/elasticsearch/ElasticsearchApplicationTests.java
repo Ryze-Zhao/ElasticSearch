0c2a40d39be283c02c762089cc0abe32976bb0d8
RyzeZhao <643080112@qq.com>
2019-07-25 16:23:21 +0800

    7.3    ​测试代码

--- src/test/java/com/zhaostudy/elasticsearch/ElasticsearchApplicationTests.java
+++ src/test/java/com/zhaostudy/elasticsearch/ElasticsearchApplicationTests.java
@@ -1,16 +1,24 @@
 package com.zhaostudy.elasticsearch;
 
+import com.zhaostudy.elasticsearch.repository.domain.Book;
+import com.zhaostudy.elasticsearch.service.EsService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ElasticsearchApplicationTests {
-
+    @Autowired
+    private EsService esService;
     @Test
-    public void contextLoads() {
+    public void createIndex() {
+        esService.createIndex(Book.class);
     }
-
+    @Test
+    public void deleteIndex() {
+        esService.deleteIndex(Book.class);
+    }
 }
